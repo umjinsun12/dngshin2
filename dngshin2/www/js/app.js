dd18b012f662e3ff8673fb9a0bea83f54093c2c1
umjinsun12 <umjinsun12@gmail.com>
2016-08-15 20:44:21 +0900

    .

--- dngshin2/www/js/app.js
+++ dngshin2/www/js/app.js
@@ -13,7 +13,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
